47a5c61b2c4eb9908e66dab680811ccaadd32ce9
Speck <stephan.speck21@gmail.com>
2023-04-25 13:14:09 -0500

    S1

--- src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -2,10 +2,16 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Chris Bay
+ */
 @Controller
 public class HomeController {
+
     @GetMapping
-    public String index(){
+    public String index() {
         return "index";
     }
+
 }
